d71055984d569a3f3a5d361f49be4dcf5d132fd7
KostantinRudenko <rudenkokostya20@gmail.com>
2024-08-06 12:40:18 +0300

    Made man move to mouse position if it was clicked

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@ int main()
 
     Vector2i VF(0, 0);
     Vector2f D(100, 100);
+    Vector2i mousePosition(0, 0);
+    Vector2f mousePositionF(0, 0);
+    Vector2f spritePos(0, 0);
+
+    float dist = 0;
 
     Texture t;
     t.loadFromFile("./NPC/Miner/minerAnim-Sheet.png");
     Man man(t, 32, 48);
     man.setPosition(Vector2f(0, 0));
+    man.setFrame(VF);
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
+    sf::RenderWindow window(sf::VideoMode(1366, 786), "Game");
 
     while (window.isOpen())
     {
@@ -29,21 +35,22 @@ int main()
                 window.close();
         }
 
+        if (Mouse::isButtonPressed(Mouse::Button::Left))
+        {
+            mousePosition = Mouse::getPosition();
+            mousePositionF.x = mousePosition.x;
+            mousePositionF.y = mousePosition.y;
+        }
+
         VF.x %= 5;
-        Vector2f spritePos = man.getPosition();
-        float dist = math.getDistance(spritePos - D);
-        cout << dist << endl;
+        spritePos = man.getPosition();
+        dist = math.getDistance(spritePos - mousePositionF);
         window.clear();
         man.setFrame(VF);
         
         if (dist > 1)
         {
-            man.setPosition(spritePos + math.getNormalised(D - spritePos));
-        }
-        else
-        {
-            cin >> D.x;
-            cin >> D.y;
+            man.setPosition(spritePos + math.getNormalised(mousePositionF - spritePos));
         }
         man.drawOn(window);
         window.display();
